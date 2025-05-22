7112d494a881d86cf4231dc933346c0f6938a545
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-22 19:12:40 +0530

    Refactor CSS layout and enhance HTML structure; add new employee section and improve search functionality in JavaScript

--- manage.js
+++ manage.js
@@ -25,17 +25,39 @@ val
   .catch((v) => console.log(v));
 function searchFunc(e) {
   const value = e.target.value;
-  arr.filter((o) => {
-    var intval = parseInt(o.id);
-    // if()
+  let m = arr.filter((o) => {
     if (
       o.id.includes(value) ||
-      o.name.includes(value) ||
-      o.department.includes(value) ||
-      o.gender.includes(value)
+      o.name.toLowerCase().includes(value) ||
+      o.department.toLowerCase().includes(value) ||
+      o.gender.toLowerCase().includes(value)
     ) {
       return true;
     }
     return false;
+  });
+  document.getElementsByClassName("tb")[0].innerHTML = `<table class="tb">
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>GENDER</th>
+          <th>DEPARTMENT</th>
+          <th>SALARY</th>
+          <th>SDATE</th>
+        </tr>
+      </table>`;
+  m.map((o) => {
+    document.getElementsByClassName("tb")[0].innerHTML += `<tr>
+            <td>${o.id}</td>
+            <td>${o.name}</td>
+            <td>${o.gender}</td>
+            <td>${o.department}</td>
+            <td>${o.salary}</td>
+            <td>${o.startdate}</td>
+            <td>
+            <img src="./edit-3-svgrepo-com.svg" alt="" class="svg" />
+            <img src="./delete-1487-svgrepo-com.svg" alt="" class="svg" />
+          </td>
+        </tr>`;
   });
 }
